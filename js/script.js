20198fdf7b996509baaf1b390a5c2b40a6754817
Omar Saucedo <omaroso27@gmail.com>
2020-02-19 14:02:12 -0600

    funcion de mover piezas con bug

--- js/script.js
+++ js/script.js
@@ -9,12 +9,14 @@ var selectedPiece = null;
 createBoard();
 createPieces();
 
-function createCell() {
+function createCell(position) {
 	var cellElement = document.createElement("div");
 	cellElement.style.width = width;
 	cellElement.style.height = height;
 	cellElement.style.border = "1px solid black";
 	cellElement.style.backgroundColor = "#124242";
+	cellElement.dataset.position = position;
+	cellElement.dataset.full = false;
 	cellElement.onclick = cellOnClick;
 	return cellElement;
 };
@@ -30,13 +32,14 @@ function createPiece(piece, width, height) {
 	//pieceElement.style.border = "1px solid black";
 	pieceElement.onclick = pieceOnClick;
 	pieceElement.src = piece.image;
+	pieceElement.dataset.position = piece.position;
 	cellElement.appendChild(pieceElement);
 	return cellElement;
 };
 
 function createBoard() {
 	for (var i = 0; i < 16; i++) {
-		let cellElement = createCell();
+		let cellElement = createCell(i+1);
 		addCell(cellElement);
 	}
 };
@@ -65,7 +68,7 @@ function generatePieceData() {
 		for (var j = 1; j < 5; j++) {
 			let piece = {
 				image: "img/" + i.toString() + j.toString() + ".jpg",
-				position: (j-1) + (i-1)*4,
+				position: (j) + (i-1)*4,
 			};
 			pieces.push(piece);
 		}
@@ -74,18 +77,47 @@ function generatePieceData() {
 };
 
 function pieceOnClick(e) {
+	// console.log(e.target.tagName);
+	// if (e.target.tagName.toUpperCase() != "IMG" || selectedPiece) {
+	// 	console.log("nada");
+	// 	return;
+	// };
+	console.log("new piece piece")
 	var piece = e.target;
 	selectedPiece = piece;
 };
 
 function cellOnClick(e) {
 	let selectedCell = e.target;
+	//console.log(selectedCell);
 	if (selectedPiece) {
-		selectedCell.appendChild(selectedPiece);
+		if (selectedCell.tagName.toUpperCase() == "DIV") {
+			selectedPiece.onclick = null;
+			selectedCell.appendChild(selectedPiece);
+			selectedCell.dataset.full = "true";
+		}
+		else {
+			if (selectedPiece == selectedCell) {
+				selectedPiece.onclick = pieceOnClick;
+				containerPiece.getElementsByTagName("div")[selectedCell.dataset.position-1].appendChild(selectedCell);
+			}
+			else {
+				let parentDiv = selectedCell.parentNode;
+				selectedCell.onclick = pieceOnClick;
+				containerPiece.getElementsByTagName("div")[selectedCell.dataset.position-1].appendChild(selectedCell);
+				parentDiv.appendChild(selectedPiece);
+			}
+		}
 		selectedPiece = null;
 	}
 	else {
-		console.log("No hay pieza seleccionada");
+		if (selectedCell.tagName.toUpperCase() == "DIV") {
+			console.log("No hay pieza seleccionada");
+		}
+		else {
+			console.log("new piece div");
+			selectedPiece = selectedCell;
+		}
 	}
 };
 
